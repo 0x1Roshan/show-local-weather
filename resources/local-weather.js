dd2480596f53ed0ca1e25dc4a7943f62b9c185e9
Roshan <rcroshan63@gmail.com>
2017-04-08 17:09:54 +0545

    added location

--- resources/local-weather.js
+++ resources/local-weather.js
@@ -1,10 +1,31 @@
-$(document).ready(function() {
-	if ("geolocation" in navigator){  
-        	navigator.geolocation.getCurrentPosition(function(position) { 
-        		console.log(position.coords.latitude);
-                // $("#result").html("Found your location <br />Lat : "+position.coords.latitude+" </br>Lang :"+ position.coords.longitude);
-            });
-    }else{
-        console.log("Browser doesn't support geolocation!");
-    }
+$( document ).ready( function() {
+	var latitude;
+	var longitude;
+	var city;
+	var country;
+
+	$.ajax({
+		dataType: "json",
+		url: "http://ip-api.com/json",
+		success: function( data ) {
+			latitude = data.lat;
+			longitude = data.lon;
+		
+			putLocation( data );
+
+			$.ajax({
+				dataType: "json",
+				url:"http://api.openweathermap.org/data/2.5/weather?lat="
+				+latitude+"&lon="+ longitude + "&appid=fe99cd3d99dc8b934cb6774f84389ab5",				
+				success: function( response ) {
+					console.log( response );
+				}	
+			});
+		},
+	});
 });
+
+function putLocation( data ) {
+	$(".city").html( data.city );
+	$(".country").html( data.country );
+}
